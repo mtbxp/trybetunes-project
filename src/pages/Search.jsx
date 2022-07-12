69b281cbed13050fa5cccabfe332db0a9e8d4b8e
mtbxp <marcellotb2@gmail.com>
2022-07-12 04:55:11 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -7,6 +9,8 @@ class Search extends React.Component {
     this.state = {
       artist: '',
       disable: true,
+      artistName: '',
+      fetchAlbums: [],
     };
   }
 
@@ -25,8 +29,18 @@ class Search extends React.Component {
     });
   }
 
+  onClick = async () => {
+    const { artist } = this.state;
+    const artistsList = await searchAlbumsAPI(artist);
+    this.setState({
+      artistName: artist,
+      artist: '',
+      fetchAlbums: artistsList,
+    });
+  }
+
   render() {
-    const { artist, disable } = this.state;
+    const { artist, disable, fetchAlbums, artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -40,12 +54,36 @@ class Search extends React.Component {
         />
         <button
           data-testid="search-artist-button"
-          type="button"
+          type="submit"
           disabled={ disable }
+          onClick={ this.onClick }
         >
           Pesquisar
-
         </button>
+
+        {
+          fetchAlbums.length > 0 ? (
+            <div>
+              <h2>{ `Resultado de álbuns de: ${artistName}` }</h2>
+              {
+                fetchAlbums.map((album) => {
+                  const { collectionName, artworkUrl100, collectionId } = album;
+                  return (
+                    <div key={ collectionId }>
+                      <h4>{ collectionName }</h4>
+                      <img src={ artworkUrl100 } alt="Capa do Album" />
+                      <Link
+                        data-testid={ `link-to-album-${collectionId}` }
+                        to={ `/album/${collectionId}` }
+                      />
+                    </div>
+                  );
+                })
+              }
+            </div>
+          )
+            : <h2>Nenhum álbum foi encontrado</h2>
+        }
       </div>
     );
   }
