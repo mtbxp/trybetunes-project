7e085293667569d6adaa49d091af0b1af9098a86
mtbxp <marcellotb2@gmail.com>
2022-07-11 23:05:21 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,50 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artist: '',
+      disable: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      artist: value,
+    }, this.enabledisableButton);
+  }
+
+  enabledisableButton = () => {
+    const { artist } = this.state;
+    const min = 2;
+    this.setState({
+      disable: artist.length < min,
+    });
+  }
+
   render() {
+    const { artist, disable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Pesquisar</h1>
+        <input
+          type="text"
+          value={ artist }
+          placeholder="Nome do Artista"
+          data-testid="search-artist-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ disable }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
